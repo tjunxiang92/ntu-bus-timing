b7fd9f691b208668e45b43c3c7063bf177ce98b0
Tan Jun Xiang <tjunxiang92@gmail.com>
2023-10-01 13:25:32 +0800

    Added JS Files

---
+++ readability.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - @mozilla/readability@0.4.4
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/@mozilla/readability
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/@mozilla/readability@v0.4.4-6s8w04G3M0iOso42kr4t/mode=imports/optimized/@mozilla/readability.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/@mozilla/readability@v0.4.4-6s8w04G3M0iOso42kr4t/mode=imports,min/optimized/@mozilla/readability.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/@mozilla/readability@v0.4.4-6s8w04G3M0iOso42kr4t/dist=es2019,mode=imports/optimized/@mozilla/readability.js';
+export {default} from '/-/@mozilla/readability@v0.4.4-6s8w04G3M0iOso42kr4t/dist=es2019,mode=imports/optimized/@mozilla/readability.js';
